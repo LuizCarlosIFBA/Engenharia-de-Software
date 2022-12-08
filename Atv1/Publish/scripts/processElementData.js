70ff95ed8f23052cf91c3f1178f4d0b6ebda293c
LuizCarlosIFBA <sacramento.tecnico@hotmail.com>
2022-12-08 17:40:04 -0300

    ok

---
+++ Atv1/Publish/scripts/processElementData.js
@@ -0,0 +1,69 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2005, 2006 IBM Corporation and others.
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+// 
+// Contributors:
+// IBM Corporation - initial implementation
+//------------------------------------------------------------------------------
+
+// This file will include dynamically generated process layout related data from 
+// the publishing service 
+contentPage.processPage.elementUrls["_O9Bwx16JEe2vKOZOnMo4aA"] = ["Demanda","SistemaConsulta/deliveryprocesses/demanda_56524F4C.html"];
+contentPage.processPage.elementUrls["_2bc4Yl6IEe2vKOZOnMo4aA"] = ["Partes Interessadas","SistemaConsulta/deliveryprocesses/partes_interessadas_9590E7E7.html"];
+contentPage.processPage.elementUrls["_NjIloF6JEe2vKOZOnMo4aA"] = ["Fazer manutencao do banco de dados","SistemaConsulta/deliveryprocesses/fazer_manutencao_do_banco_de_dados_6CFC7EAA.html"];
+contentPage.processPage.elementUrls["_XLww8V6CEe2vKOZOnMo4aA"] = ["Entrega Parcial","SistemaConsulta/deliveryprocesses/Entrega Parcial_3F61FBF.html"];
+contentPage.processPage.elementUrls["_MELUUF6JEe2vKOZOnMo4aA"] = ["Gerar codigo do banco","SistemaConsulta/deliveryprocesses/gerar_codigo_do_banco_5A210C06.html"];
+contentPage.processPage.elementUrls["_PnC-IF6JEe2vKOZOnMo4aA"] = ["Testar codigo do sistema","SistemaConsulta/deliveryprocesses/testar_codigo_do_sistema_740B2499.html"];
+contentPage.processPage.elementUrls["_SZPY916JEe2vKOZOnMo4aA"] = ["Demanda","SistemaConsulta/deliveryprocesses/demanda_411C7E5C.html"];
+contentPage.processPage.elementUrls["_O9BwwF6JEe2vKOZOnMo4aA"] = ["Gerar codigo do sistema","SistemaConsulta/deliveryprocesses/gerar_codigo_do_sistema_2FAFA342.html"];
+contentPage.processPage.elementUrls["_9y_Gp16IEe2vKOZOnMo4aA"] = ["Documento de Analise de Requisitos","SistemaConsulta/deliveryprocesses/documento_de_analise_de_requisitos_1C0A1F3C.html"];
+contentPage.processPage.elementUrls["_gJEDIV6CEe2vKOZOnMo4aA"] = ["Implantação","SistemaConsulta/deliveryprocesses/Implantação_A3BCD01C.html"];
+contentPage.processPage.elementUrls["__L4MMF6IEe2vKOZOnMo4aA"] = ["Escrever historia do usuario","SistemaConsulta/deliveryprocesses/escrever_historia_do_usuario_84CD3F92.html"];
+contentPage.processPage.elementUrls["_crPRYV6CEe2vKOZOnMo4aA"] = ["Persistência","SistemaConsulta/deliveryprocesses/Persistência_FDEFF25B.html"];
+contentPage.processPage.elementUrls["_2bc4Zl6IEe2vKOZOnMo4aA"] = ["Documento de Analise de Requisitos","SistemaConsulta/deliveryprocesses/documento_de_analise_de_requisitos_5A22CA06.html"];
+contentPage.processPage.elementUrls["_SZPY9V6JEe2vKOZOnMo4aA"] = ["Codigo Fonte","SistemaConsulta/deliveryprocesses/codigo_fonte_E9A99481.html"];
+contentPage.processPage.elementUrls["_9y_GpV6IEe2vKOZOnMo4aA"] = ["Estoria do Usuario","SistemaConsulta/deliveryprocesses/estoria_do_usuario_C4973561.html"];
+contentPage.processPage.elementUrls["_SZOx4F6JEe2vKOZOnMo4aA"] = ["Analisar codigo do sistema","SistemaConsulta/deliveryprocesses/analisar_codigo_do_sistema_83249D6.html"];
+contentPage.processPage.elementUrls["_O9BwyV6JEe2vKOZOnMo4aA"] = ["Diagrama de Classes","SistemaConsulta/deliveryprocesses/diagrama_de_classes_C3714790.html"];
+contentPage.processPage.elementUrls["_SZPY8V6JEe2vKOZOnMo4aA"] = ["Suporte","SistemaConsulta/deliveryprocesses/suporte_2517B262.html"];
+contentPage.processPage.elementUrls["_LMSDAV6JEe2vKOZOnMo4aA"] = ["Analista de Banco de Dados","SistemaConsulta/deliveryprocesses/analista_de_banco_de_dados_94B791DB.html"];
+contentPage.processPage.elementUrls["_LMSDBV6JEe2vKOZOnMo4aA"] = ["Codigo Fonte","SistemaConsulta/deliveryprocesses/codigo_fonte_594973FA.html"];
+contentPage.processPage.elementUrls["_bV7rUF6CEe2vKOZOnMo4aA"] = ["Gerência de backlog","SistemaConsulta/deliveryprocesses/Gerência de backlog_10483DED.html"];
+contentPage.processPage.elementUrls["_dlXXUV6CEe2vKOZOnMo4aA"] = ["Construção","SistemaConsulta/deliveryprocesses/Construção_35A560F6.html"];
+contentPage.processPage.elementUrls["_9y_Go16IEe2vKOZOnMo4aA"] = ["Partes Interessadas","SistemaConsulta/deliveryprocesses/partes_interessadas_57783D1D.html"];
+contentPage.processPage.elementUrls["_9y_GoV6IEe2vKOZOnMo4aA"] = ["Gestor Geral","SistemaConsulta/deliveryprocesses/gestor_geral_55342.html"];
+contentPage.processPage.elementUrls["_2bc4ZF6IEe2vKOZOnMo4aA"] = ["Estoria do Usuario","SistemaConsulta/deliveryprocesses/estoria_do_usuario_60EBD5E0.html"];
+contentPage.processPage.elementUrls["_2bbqQF6IEe2vKOZOnMo4aA"] = ["Examinar analise de requisitos","SistemaConsulta/deliveryprocesses/examinar_analise_de_requisitos_91C25767.html"];
+contentPage.processPage.elementUrls["_J9FE8F6JEe2vKOZOnMo4aA"] = ["Fazer estimativa de tempo de criacao das tarefas","SistemaConsulta/deliveryprocesses/fazer_estimativa_de_tempo_de_criacao_das_tarefas_A26C9A50.html"];
+contentPage.processPage.elementUrls["_SZOx4V6JEe2vKOZOnMo4aA"] = ["Equipe Tecnica de Implantacao","SistemaConsulta/deliveryprocesses/equipe_tecnica_de_implantacao_12D029E6.html"];
+contentPage.processPage.elementUrls["_2bc4YF6IEe2vKOZOnMo4aA"] = ["Gestor Geral","SistemaConsulta/deliveryprocesses/gestor_geral_9C59F3C1.html"];
+contentPage.processPage.elementUrls["_H125wF6JEe2vKOZOnMo4aA"] = ["Criar lista de tarefas pelo backlog","SistemaConsulta/deliveryprocesses/criar_lista_de_tarefas_pelo_backlog_9D35432F.html"];
+contentPage.processPage.elementUrls["_MvMAMF6JEe2vKOZOnMo4aA"] = ["Testar banco de dados","SistemaConsulta/deliveryprocesses/testar_banco_de_dados_752B2C0A.html"];
+contentPage.processPage.elementUrls["_LMSDCV6JEe2vKOZOnMo4aA"] = ["Demanda","SistemaConsulta/deliveryprocesses/demanda_1DDB5619.html"];
+contentPage.processPage.elementUrls["_t10voV7mEe2lbpxNc_K5uw"] = ["Status do Projeto","SistemaConsulta/deliveryprocesses/status_do_projeto_59F23525.html"];
+contentPage.processPage.elementUrls["_LMSDAF6JEe2vKOZOnMo4aA"] = ["Criar modelo do banco","SistemaConsulta/deliveryprocesses/criar_modelo_do_banco_8A19B1CB.html"];
+contentPage.processPage.elementUrls["_QDfCgF6JEe2vKOZOnMo4aA"] = ["Manutenir codigo do sistema","SistemaConsulta/deliveryprocesses/manutenir_codigo_do_sistema_4A55CD43.html"];
+contentPage.processPage.elementUrls["_H13g0l6JEe2vKOZOnMo4aA"] = ["Backlog","SistemaConsulta/deliveryprocesses/backlog_24DF6ECD.html"];
+contentPage.processPage.elementUrls["_LMSDB16JEe2vKOZOnMo4aA"] = ["Relatorio de Teste","SistemaConsulta/deliveryprocesses/relatorio_de_teste_B0BC5DD5.html"];
+contentPage.processPage.elementUrls["_O9BwxV6JEe2vKOZOnMo4aA"] = ["Relatorio de Teste","SistemaConsulta/deliveryprocesses/relatorio_de_teste_FEDF6571.html"];
+contentPage.processPage.elementUrls["_SZPY816JEe2vKOZOnMo4aA"] = ["Entrega Parcial","SistemaConsulta/deliveryprocesses/entrega_parcial_7C8A9C3D.html"];
+contentPage.processPage.elementUrls["_5IRrAF6IEe2vKOZOnMo4aA"] = ["Gerar documento de viabilidade","SistemaConsulta/deliveryprocesses/gerar_documento_de_viabilidade_F4D2678C.html"];
+contentPage.processPage.elementUrls["_TIwxYF6JEe2vKOZOnMo4aA"] = ["Integrar sistema","SistemaConsulta/deliveryprocesses/integrar_sistema_52EEE437.html"];
+contentPage.processPage.elementUrls["_H13g1l6JEe2vKOZOnMo4aA"] = ["Estoria do Usuario","SistemaConsulta/deliveryprocesses/estoria_do_usuario_E97150EC.html"];
+contentPage.processPage.elementUrls["_JDRvIF6JEe2vKOZOnMo4aA"] = ["Definir objetivos e resultados dos papeis","SistemaConsulta/deliveryprocesses/definir_objetivos_e_resultados_dos_papeis_73E1ED4F.html"];
+contentPage.processPage.elementUrls["_9y_GqV6IEe2vKOZOnMo4aA"] = ["Demanda","SistemaConsulta/deliveryprocesses/demanda_89291780.html"];
+contentPage.processPage.elementUrls["_9y_GoF6IEe2vKOZOnMo4aA"] = ["Examinar analise de requisitos","SistemaConsulta/deliveryprocesses/examinar_analise_de_requisitos_F5677332.html"];
+contentPage.processPage.elementUrls["_O9Bww16JEe2vKOZOnMo4aA"] = ["Codigo Fonte","SistemaConsulta/deliveryprocesses/codigo_fonte_91C06D2D.html"];
+contentPage.processPage.elementUrls["_Z4KFwV6CEe2vKOZOnMo4aA"] = ["História do usuário","SistemaConsulta/deliveryprocesses/História do usuário_1523A841.html"];
+contentPage.processPage.elementUrls["_O9BwwV6JEe2vKOZOnMo4aA"] = ["Time de Desenvolvimento e Producao","SistemaConsulta/deliveryprocesses/time_de_desenvolvimento_e_producao_3A4D8352.html"];
+contentPage.processPage.elementUrls["_2bc4aF6IEe2vKOZOnMo4aA"] = ["Demanda","SistemaConsulta/deliveryprocesses/demanda_C0E904B9.html"];
+contentPage.processPage.elementUrls["_LMSDA16JEe2vKOZOnMo4aA"] = ["Modelagem de Banco de Dados","SistemaConsulta/deliveryprocesses/modelagem_de_banco_de_dados_EC2A7BB6.html"];
+contentPage.processPage.elementUrls["_H13g1F6JEe2vKOZOnMo4aA"] = ["Demanda","SistemaConsulta/deliveryprocesses/demanda_F03A5CC6.html"];
+contentPage.processPage.elementUrls["_fWGVIV6CEe2vKOZOnMo4aA"] = ["Entrega Final","SistemaConsulta/deliveryprocesses/Entrega Final_63A195A.html"];
+contentPage.processPage.elementUrls["_t1h0sV7mEe2lbpxNc_K5uw"] = ["Status do Projeto","SistemaConsulta/deliveryprocesses/status_do_projeto_DB09E7A3.html"];
+contentPage.processPage.elementUrls["_3q8xYF6IEe2vKOZOnMo4aA"] = ["Constatar riscos de criacao do sistema","SistemaConsulta/deliveryprocesses/constatar_riscos_de_criacao_do_sistema_98E9063E.html"];
+contentPage.processPage.elementUrls["_YJC4kV6CEe2vKOZOnMo4aA"] = ["Estudo de viabilidade","SistemaConsulta/deliveryprocesses/Estudo de viabilidade_B9E2DABA.html"];
+contentPage.processPage.elementUrls["_H13g0F6JEe2vKOZOnMo4aA"] = ["Gestor Geral","SistemaConsulta/deliveryprocesses/gestor_geral_2BA87AA7.html"];
